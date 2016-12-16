442bcd8011213f1a084710ef65659a133c961cc6
ywenc <yaqing@yaqings-MacBook-Pro.local>
2016-12-15 23:27:36 -0500

    Add some styling for personal page

--- frontend/components/channel/messages/messages_index.jsx
+++ frontend/components/channel/messages/messages_index.jsx
@@ -21,24 +21,25 @@ class MessagesIndex extends Component {
   }
 
   render() {
+    if (this.props.fetching) return null;
+
     const { currentChannel, currentUser, messages } = this.props;
 
     const personalPage = () => {
       if (currentChannel.name === currentUser.username ) {
         return (
-          <section className="personalPage-page">
-            <div>
-              <ul>
-                <img src={ currentUser.img_url }/>
-              </ul>
-              <ul>
+          <section className="personal-page group">
+            <div className="group">
+              <img
+                className="icon3"
+                src={ currentUser.img_url }/>
+              <ul className="group">
                 <li>{ currentUser.name }</li>
                 <li>{ `@${currentUser.username}` }</li>
               </ul>
-
             </div>
-            <p>
-              <strong>{ "This is your space." }</strong>
+            <p className="group">
+              <strong>{ "This is your space. " }</strong>
               { "Draft messages, list your to-dos, or keep links and files handy. You can also talk to yourself here, but please bear in mind you'll have to supply both sides of the conversation." }
             </p>
           </section>
@@ -47,16 +48,19 @@ class MessagesIndex extends Component {
     };
 
     const snackbearPage = () => {
-      if (this.props.router.params.channelName === '@snackbear' ) {
+      if (this.props.router.params.channelName === '@snackbear' &&
+      !this.props.fetching) {
         const snackbear = currentChannel.members.filter((member) =>
           member.username === "snackbear");
+
         return (
-          <section>
-          <p className="snackbear-page">
-            <img/>
+          <section className="snackbear-page">
             <h1>{ "Hi, Snackbear here!"}</h1>
-            { "You can ask me simple questions about what snacks I like. For example: 'do you like gummy bears?' Or simply: 'gummy bears'. I'm only a bear, but I'll do my best to answer! If I don't understand, I'll search the help center" }
-          </p>
+            <img className="icon2"
+              src={ snackbear[0].img_url }/>
+            <p>
+              { "You can ask me simple questions about what snacks I like. For example: 'do you like gummy bears?' Or simply: 'gummy bears'. I'm only a bear, but I'll do my best to answer! If I don't understand, I'll search the help center" }
+            </p>
           </section>
         );
       }
@@ -67,19 +71,21 @@ class MessagesIndex extends Component {
       <section className="messages group">
         { personalPage() }
         { snackbearPage() }
-        <section className="messages-index group">
-        { messages.map((message, idx) => {
-          return (
 
-            <ul className="message group"
-              key={ idx }>
-              <img className="icon2"
-                src={ message.author.username.img_url }></img>
-              <h4>{ message.author.username }</h4>
-              <li>{ message.body }</li>
-            </ul>
-          );
-        }) }
+        <section className="messages-index group">
+          { messages.map((message, idx) => {
+            return (
+              <ul className="message group"
+                key={ idx }>
+                <img className="icon2"
+                  src={ message.author.username.img_url }></img>
+                <h4>{ message.author.username }</h4>
+                <li>{ message.body }</li>
+              </ul>
+            );
+            })
+          }
+
           <div id="message-bottom"></div>
         </section>
       </section>
@@ -92,7 +98,7 @@ const mapStateToProps = ((state, ownProps) => {
     currentUser: state.session.currentUser,
     currentChannel: state.currentChannel.currentChannel,
     messages: allMessages(state),
-    users: state.users
+    fetching: state.fetching
   };
 });
 
--- frontend/reducers/fetching_reducer.js
+++ frontend/reducers/fetching_reducer.js
@@ -4,19 +4,29 @@ import {
   RECEIVE_ERRORS,
   RESET_ERRORS,
   FETCH_CHANNELS,
-  CREATE_CHANNEL
+  CREATE_CHANNEL,
+  JOIN_CHANNEL
 } from '../actions/channel_actions';
 
 import {
-  FETCH_USERS
+  FETCH_USERS,
+  EDIT_PROFILE,
+  RECEIVE_USERS
 } from '../actions/user_actions';
 
 import {
   FETCH_CURRENT_CHANNEL,
   GOTO_CHANNEL,
   FETCH_MESSAGES,
-  RECEIVE_MESSAGES
+  RECEIVE_MESSAGES,
+  CREATE_MESSAGES
 } from '../actions/messages_actions';
+
+import {
+  SIGNUP_USER,
+  SIGNIN_USER,
+  RECEIVE_CURRENT_USER
+} from '../actions/session_actions';
 
 const fetchingReducer = (state = false, action) => {
   Object.freeze(state);
@@ -27,6 +37,12 @@ const fetchingReducer = (state = false, action) => {
     case CREATE_CHANNEL:
     case FETCH_CURRENT_CHANNEL:
     case FETCH_MESSAGES:
+    case JOIN_CHANNEL:
+    case CREATE_MESSAGES:
+    case SIGNUP_USER:
+    case SIGNIN_USER:
+    case FETCH_USERS:
+    case EDIT_PROFILE:
       return true;
     case RECEIVE_CHANNEL:
     case RECEIVE_CHANNELS:
@@ -34,6 +50,8 @@ const fetchingReducer = (state = false, action) => {
     case RESET_ERRORS:
     case GOTO_CHANNEL:
     case RECEIVE_MESSAGES:
+    case RECEIVE_CURRENT_USER:
+    case RECEIVE_USERS:
       return false;
     default:
       return state;
