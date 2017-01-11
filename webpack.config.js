469d36edd9f6391f6a6cf8487b5425ecc9dc2b03
Charlotte Wen <cyaqingwen@gmail.com>
2017-01-10 21:11:13 -0500

    Save minification stuff for later

--- webpack.config.js
+++ webpack.config.js
@@ -10,19 +10,6 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    })
-  ],
   module: {
     loaders: [
       {
